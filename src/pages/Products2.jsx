14e9e2d75c0c80ac0cc86f467880f16dd53a2a13
pedrobailo <pedro.bailo@hotmail.com>
2024-10-21 20:07:04 -0300

    Se agregaron estilos a los productos

--- src/pages/Products2.jsx
+++ src/pages/Products2.jsx
@@ -1,5 +1,6 @@
 // Products2.jsx
 import React, { useState, useEffect } from 'react';
+import './Products2.css'; // Importa el CSS
 import Sidebar from '../components/layouts/sidebar/Sidebar';
 import Header from '../components/layouts/header/Header';
 import Footer from '../components/layouts/footer/Footer';
@@ -15,10 +16,8 @@ const Products2 = () => {
   });
 
   useEffect(() => {
-    // Aplica el filtrado en función de los filtros seleccionados
     const { category, subcategory, subSubcategory } = filters;
     const filtered = productsData.filter((product) => {
-      // Verifica si el producto coincide con la categoría, subcategoría y sub-subcategoría seleccionadas
       const matchesCategory = !category || product.category.toLowerCase() === category.toLowerCase();
       const matchesSubcategory = !subcategory || product.subcategory.toLowerCase() === subcategory.toLowerCase();
       const matchesSubSubcategory = !subSubcategory || product.subSubcategory.toLowerCase() === subSubcategory.toLowerCase();
@@ -29,7 +28,6 @@ const Products2 = () => {
   }, [filters]);
 
   const handleFilterChange = (newFilters) => {
-    // Actualiza los filtros seleccionados
     setFilters((prevFilters) => ({ ...prevFilters, ...newFilters }));
   };
 
@@ -37,14 +35,14 @@ const Products2 = () => {
     <>
       <Header />
       <Navbar />
-      <div style={{ display: 'flex' }}>
+      <div className="container">
         <Sidebar onFilterChange={handleFilterChange} />
-        <div style={{ marginLeft: '20px', flexGrow: 1 }}>
+        <div className="product-list">
           <h2>Productos</h2>
           {filteredProducts.length > 0 ? (
             <ul>
               {filteredProducts.map((product) => (
-                <li key={product.id}>{product.name}</li>
+                <li key={product.id} className="product-item">{product.name}</li>
               ))}
             </ul>
           ) : (
